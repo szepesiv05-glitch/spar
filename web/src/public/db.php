dd513d9702db6690f1a762e4156d4e4a8e7792af
szepesiv05-glitch <szepesiv05@gmail.com>
2026-04-20 19:02:42 +0200

    Update db.php

---
+++ web/src/public/db.php
@@ -0,0 +1,15 @@
+<?php
+$conn = new mysqli(
+    hostname:"127.0.0.1", 
+    username: "root",
+    password: "root",
+    database: "spar_db_data"
+);
+
+if($conn->connect_error) {
+    die("Kapcsolódási hiba");
+}
+
+$conn -> set_charset("utf8mb4");
+
+?>
