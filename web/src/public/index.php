cbc2f42c1eddfd7741cf45635df259d0950132e6
szepesiv05-glitch <szepesiv05@gmail.com>
2026-04-20 19:02:48 +0200

    Update index.php

--- web/src/public/index.php
+++ web/src/public/index.php
@@ -24,7 +24,7 @@ $result = $lekerdezes->get_result();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Beosztáskezelő</title>
 </head>
 <body>
     <h1>Elég a bűnözésből</h1>
