23a4e3d40a8e7e19a20781b8bf23d6ac2f86b014
szepesiv05-glitch <szepesiv05@gmail.com>
2026-04-20 17:54:43 +0200

    Update register.php

---
+++ web/src/public/register.php
@@ -0,0 +1,35 @@
+<?php
+require_once("db.php");
+$hiba = "";
+$siker = "";
+
+$username = trim($_POST["username"] ?? "");
+$password = trim($_POST["password"] ?? "");
+
+if ($username == "" || $password == "") {
+    $hiba = "Minden adatot meg kell adni";
+
+} else {
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $lekerdezes = $conn -> prepare("INSERT INTO users (username, password) VALUES (?,?)");
+    $lekerdezes -> bind_param("ss", $username, $hash);
+
+    if($lekerdezes -> execute()) {
+        $siker = "Sikeres regisztráció";
+    } else {
+        $hiba = "A felhasználó már foglalt";
+    }
+}
+?>
+
+<h1>Regisztráció</h1>
+<form method="post">
+    <label>Felhaszálónev: <input type="text" name="username"></label>
+    <label>Jelszó: <input type="password" name="password"></label>
+    <button type="submit">Regisztráció</button>
+</form>
+<p style="color: red"><?= $hiba ?></p>
+<p style="color: green"><?= $siker ?></p>
+
+<a href="login.php">Bejelentkezés</a>
