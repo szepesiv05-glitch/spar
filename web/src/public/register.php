94972c3fc7f8a1e66e69ebb89860146fb2350a48
szepesiv05-glitch <szepesiv05@gmail.com>
2026-04-23 17:39:14 +0200

    Update register.php

--- web/src/public/register.php
+++ web/src/public/register.php
@@ -5,15 +5,17 @@ $siker = "";
 
 $username = trim($_POST["username"] ?? "");
 $password = trim($_POST["password"] ?? "");
+$email = trim($_POST["email"] ?? "");
+$fullname = trim($_POST["fullname"] ?? "");
 
-if ($username == "" || $password == "") {
+if ($username == "" || $password == "" || $email == "" || $fullname == "") {
     $hiba = "Minden adatot meg kell adni";
 
 } else {
     $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $lekerdezes = $conn -> prepare("INSERT INTO users (username, password) VALUES (?,?)");
-    $lekerdezes -> bind_param("ss", $username, $hash);
+    $lekerdezes = $conn -> prepare("INSERT INTO users (username, password, email, full_name) VALUES (?,?,?,?)");
+    $lekerdezes -> bind_param("ssss", $username, $hash, $email, $fullname);
 
     if($lekerdezes -> execute()) {
         $siker = "Sikeres regisztráció";
@@ -25,11 +27,13 @@ if ($username == "" || $password == "") {
 
 <h1>Regisztráció</h1>
 <form method="post">
-    <label>Felhaszálónev: <input type="text" name="username"></label>
-    <label>Jelszó: <input type="password" name="password"></label>
+    <label>Email: <input type="email" name="email" required></label>
+    <label>Felhaszálónev: <input type="text" name="username" required></label>
+    <label>Teljes név: <input type="text" name="fullname" required></label>
+    <label>Jelszó: <input type="password" name="password" required></label>
     <button type="submit">Regisztráció</button>
 </form>
 <p style="color: red"><?= $hiba ?></p>
 <p style="color: green"><?= $siker ?></p>
 
-<a href="login.php">Bejelentkezés</a>
+<p>Már van fiókod? <a href="login.php">Bejelentkezés</a></p>
