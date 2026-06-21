d331f111ff09e40dc6c7c473b1b7ef9869362ecd
szepesiv05-glitch <szepesiv05@gmail.com>
2026-06-21 03:22:10 +0200

    Update auth.php

--- web/src/includes/auth.php
+++ web/src/includes/auth.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 
+// ellenőrizzük hogy a fekhasználó be van-e jelentkezve
 function Bejelentkezve(): bool {
     return isset($_SESSION["user_id"]);
 }
 
+// ha nincs akkor átirányítjuk a login oldalra
 function csakBejelentekzve() : void {
     if(!Bejelentkezve()) {
         header("Location: login.php");
